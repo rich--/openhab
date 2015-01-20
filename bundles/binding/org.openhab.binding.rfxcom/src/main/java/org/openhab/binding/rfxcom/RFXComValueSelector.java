b21352b59d7cfcd3d1fc7d927db8e41591041c66
vagrant <vagrant@precise32.(none)>
2015-01-20 15:51:34 +0000

    First add of UV Message for Oregon Sensors

--- bundles/binding/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/RFXComValueSelector.java
+++ bundles/binding/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/RFXComValueSelector.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2015, openHAB.org and others.
+ * Copyright (c) 2010-2014, openHAB.org and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -53,7 +53,9 @@ public enum RFXComValueSelector {
 	MOTION("Motion", SwitchItem.class),
 	CONTACT("Contact", ContactItem.class),
 	VOLTAGE("Voltage", NumberItem.class),
-	SET_POINT("SetPoint", NumberItem.class)
+	SET_POINT("SetPoint", NumberItem.class),
+	UV_LEVEL("UVLevel", NumberItem.class),
+	UV_DESCRIPTION("UVDescription", StringItem.class)
 	;
 
 	private final String text;
